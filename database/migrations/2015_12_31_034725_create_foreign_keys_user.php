aa72d52592736948ec9378d318072863aa7e4878
Ochir Sanjaadorj <osanjaadorj@bluefountainmedia.com>
2015-12-31 13:05:20 +0800

    fix for conflict

--- database/migrations/2015_12_31_034725_create_foreign_keys_user.php
+++ database/migrations/2015_12_31_034725_create_foreign_keys_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Eloquent\Model;
 
-class CreateForeignKeys extends Migration {
+class CreateForeignKeysUser extends Migration {
 
 	public function up()
 	{

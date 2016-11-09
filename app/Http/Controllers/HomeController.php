f5b2e7816569113b753a7f8cb0bbf186a6097585
Ochir Sanjaadorj <sor.net@gmail.com>
2016-11-09 18:45:53 +0800

    change slides

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
     public function index(){
 		//Slideshow
 		$slideshow = [
-			['title'=>'Туршилтын нээлт явагдаж байна','desc'=>'Төслийн туршилын хугацаа эхлэж байна.','slide'=>asset('images/homeslides/main.jpg'),'cta'=>'Яг одоо өөрийнхөө төслийг оруулах','url'=>url('project/add')],
+			['title'=>'Туршилтын нээлт явагдаж байна','desc'=>'Төслийн туршилын хугацаа эхлэж байна.','slide'=>asset('images/homeslides/staging.jpg'),'cta'=>'Алдааны мэдээлэл илгээх','url'=>'https://docs.google.com/forms/d/e/1FAIpQLSd_DYqUn2EPkDblQpqr93mcJT5bvD40wgmaPKyHRXmvq1GrxQ/viewform?c=0&w=1'],
 			['title'=>'BOOTUP.mn гэж юу вэ?','desc'=>'BootUP.mn бол мэдээллийн технологийн шинэлэг санаатай залууст эхний ээлжийн хөрөнгө оруулалтыг сайтаараа дамжуулан олон нийтээс босгох веб сайт юм. Бид зөвхөн эхний хөрөнгө оруулалт босгоод зогсохгүй зөвлөгөө өгч төслийг үр дүнтэй болгох талаас нь дэмжин ажиллах бөгөөд хөрөнгө оруулалтын хугацаа дуусмагц дараагийн хөрөнгө оруулагчтай холбох суваг нь болно. Танилцуулга материалыг <a href="https://dl.dropboxusercontent.com/u/46338286/bootup.pdf" target="_blank">эндээс татаарай</a>.','slide'=>asset('images/homeslides/main.jpg'),'cta'=>'Яг одоо өөрийнхөө төслийг оруулах','url'=>url('project/add')],
 		];
 
